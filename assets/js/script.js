fb4c664be8cf94857d136e2da167f9624d756379
alexdotrud <rudenko530@gmail.com>
2025-04-28 19:42:10 +0200

    Add pictureas of Aurora Rick and Dog

---
+++ assets/js/script.js
@@ -0,0 +1,16 @@
+const storyContainer = document.getElementById("story-container");
+const picturesContainer = document.getElementById("pictures-container");
+const choicesContainer = document.getElementById("choices-container");
+
+$(document).ready(function() {
+$(",meet-characters").click(function Characters(){
+$(".story-heading").text(`<h1>Meet the Explorers</h1>`);
+$(".intro").hide();
+$("#pictures-container").html(`<img src="assets/images/aurora.png" class="characters-img" alt="Picture of Aurora">
+    <img src="" class="characters-img" alt="Picture of Rick">
+    <img src="" class="characters-img" alt="Picture of Dog">`)
+$("#choices-container").hide();
+})
+}
+
+);
