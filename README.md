24797f23098c29208d3344391423b0a7569c7b26
alexdotrud <rudenko530@gmail.com>
2025-04-28 21:57:35 +0200

    Change the styles to improve responsivness

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Fo creating logo -https://new.express.adobe.com/
 https://coolors.co/29339b-74a4bc-b6d6cc-f1fec6-ff3a20 -for creating color pallete.
 (https://www.w3schools.com/css/css3_gradients.asp) - for CSS/ HTML
 https://pixabay.com/ - picture of papirus.
+https://imageresizer.com/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,17 @@
-const storyContainer = document.getElementById("story-container");
-const picturesContainer = document.getElementById("pictures-container");
-const choicesContainer = document.getElementById("choices-container");
-
 $(document).ready(function() {
-    
-$(".meet-characters").click(function Characters(){
-$(".story-heading").text(`<h1>Meet the Explorers</h1>`);
-$(".intro").hide();
-$("#pictures-container").html(`<img src="assets/images/aurora.png" class="characters-img" alt="Picture of Aurora">
-    <img src="assets/images/rick.png" class="characters-img" alt="Picture of Rick">
-    <img src="assets/images/dog.png" class="characters-img" alt="Picture of Dog">`)
-$("#choices-container").hide();
-})
-}
+    $(".meet-characters").click(function () {
+    $(".story-heading").text("Meet the Explorers");
 
-);
+    $(".intro").hide();
+    $(".btn-intro").hide();
+
+    $("#pictures-container").html(`
+        <img src="assets/images/aurora.png" class="characters-img" alt="Picture of Aurora">
+        <img src="assets/images/rick.png" class="characters-img" alt="Picture of Rick">
+        <img src="assets/images/dog.png" class="characters-img" alt="Picture of Dog">
+    `);
+
+      // Clear the choices container first, then add the new button
+    $("#choices-container").html(`<button class="btn btn-start-story">Start The Story</button>`);
+    });
+});
