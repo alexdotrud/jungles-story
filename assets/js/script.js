44a73c8879821a40a6614191038e9e84f9635b82
alexdotrud <rudenko530@gmail.com>
2025-05-03 21:41:30 +0200

    Improve JS 1 function structure and give it a name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,18 @@
-$(document).ready(function() {
-    $(".meet-characters").click(function () {
-    $(".story-heading").text("Meet the Explorers");
+$(document).ready(function () {
+  function showCharacters() {
+      $(".story-heading").text("Meet the Explorers");
 
-    $(".intro").hide();
-    $(".btn-intro").hide();
+      $(".intro").hide();
+      $(".btn-intro").hide();
 
-    $("#pictures-container").html(`
-        <img src="assets/images/aurora.png" class="characters-img" alt="Picture of Aurora">
-        <img src="assets/images/rick.png" class="characters-img" alt="Picture of Rick">
-        <img src="assets/images/dog.png" class="characters-img" alt="Picture of Dog">
-    `);
+      $("#pictures-container").html(`
+          <img src="assets/images/aurora.png" class="characters-img" alt="Picture of Aurora">
+          <img src="assets/images/rick.png" class="characters-img" alt="Picture of Rick">
+          <img src="assets/images/dog.png" class="characters-img" alt="Picture of Dog">
+      `);
 
-      // Clear the choices container first, then add the new button
-    $("#choices-container").html(`<button class="btn btn-start-story">Start The Story</button>`);
-    });
+      $("#choices-container").html(`<button class="btn btn-start-story">Start The Story</button>`);
+  }
+
+  $(".meet-characters").click(showCharacters);
 });
