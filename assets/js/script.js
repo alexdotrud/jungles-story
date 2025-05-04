076be8b099511188c0c48ae158d3e5d692ed2837
alexdotrud <rudenko530@gmail.com>
2025-05-04 12:26:45 +0200

    Add audio folder and a dog bark to it

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,74 +6,75 @@ const picturesContainer = document.querySelector("#pictures-container");
 const choicesContainer = document.querySelector("#choices-container");
 const text = document.querySelector("#story-container");
 
+// Dog Bark
+const dogBark = new audio(assets/audio/bark-MediaSourceHandle.mp3)
 // Typing Effect
 function typeWriter(element, text, speed = 50, callback) {
-  let i = 0;
-
-  function type() {
-      if (i < text.length) {
-          $(element).append(text.charAt(i));
-          i++;
-          setTimeout(type, speed);
-      } else if (callback) {
-          callback();
-      }
-  }
-
-  type();
+    let i = 0;
+    element.innerHTML = '';
+    function type() {
+        if (i < text.length) {
+            element.innerHTML += text.charAt(i);
+            i++;
+            setTimeout(type, speed);
+        } else if (callback) {
+            callback();
+        }
+    }
+    type();
 }
 
 $(document).ready(function() {
 //  Meeting Characters Chapter
-  function showCharacters() {
-      $(".story-heading").text("Meet the Explorers");
+function showCharacters() {
+    $(".story-heading").text("Meet the Explorers");
 
-      $(".intro").hide();
-      $(".btn-intro").hide();
+    $(".intro").hide();
+    $(".btn-intro").hide();
 
-      $("#pictures-container").html(`
-          <img src="assets/images/aurora.png" class="characters-img" alt="Aurora" id="auroraImage">
-          <img src="assets/images/rick.png" class="characters-img" alt="Rick" id="rickImage">
-          <img src="assets/images/dog.png" class="characters-img" alt="Dog" id="dogImage">
+    $("#pictures-container").html(`
+        <img src="assets/images/aurora.png" class="characters-img" alt="Aurora" id="auroraImage">
+        <img src="assets/images/rick.png" class="characters-img" alt="Rick" id="rickImage">
+        <img src="assets/images/dog.png" class="characters-img" alt="Dog" id="dogImage">
 
-          <div id="popupModal" class="modal">
-              <div class="modal-content">
-                  <span class="close">&times;</span>
-                  <p id="modalContent">Here is your message or content!</p>
-              </div>
-          </div>
-      `);
+        <div id="popupModal" class="modal">
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <p id="modalContent">Here is your message or content!</p>
+            </div>
+        </div>
+    `);
 
-      $("#choices-container").html(`<button class="btn btn-start-story">Start The Story</button>`);
+    $("#choices-container").html(`<button class="btn btn-start-story">Start The Story</button>`);
 // Pop-Up's on images
-      $(".characters-img").click(function popUp() {
-          var imgAlt = $(this).attr("alt");
-          var infoText = "";
+    $(".characters-img").click(function popUp() {
+        var imgAlt = $(this).attr("alt");
+        var infoText = "";
 
-          if (imgAlt === "Aurora") {
-              infoText = "Aurora is a fearless young explorer driven by curiosity and a desire to uncover the world's hidden mysteries.";
-          } else if (imgAlt === "Rick") {
-              infoText = "Rick is Aurora’s loyal and adventurous best friend, always ready with his survival skills and a good sense of humor.";
-          } else if (imgAlt === "Dog") {
-              infoText = "Dog is Aurora’s faithful companion, whose sharp instincts and unwavering loyalty help guide and protect her on every adventure.";
-          }
+        if (imgAlt === "Aurora") {
+            infoText = "Aurora is a fearless young explorer driven by curiosity and a desire to uncover the world's hidden mysteries.";
+        } else if (imgAlt === "Rick") {
+            infoText = "Rick is Aurora’s loyal and adventurous best friend, always ready with his survival skills and a good sense of humor.";
+        } else if (imgAlt === "Dog") {
+            infoText = "Dog is Aurora’s faithful companion, whose sharp instincts and unwavering loyalty help guide and protect her on every adventure.";
+        }
 
-          $("#modalContent").text(infoText);
-          $("#popupModal").fadeIn();
-      });
+        $("#modalContent").text(infoText);
+        $("#popupModal").fadeIn();
+    });
 
-      $(document).on("click", ".close", function () {
-          $("#popupModal").fadeOut();
-      });
+    $(document).on("click", ".close", function () {
+        $("#popupModal").fadeOut();
+    });
 
-      $(window).click(function (event) {
-          if ($(event.target).is("#popupModal")) {
-              $("#popupModal").fadeOut();
-          }
-      });
-  }
+    $(window).click(function (event) {
+        if ($(event.target).is("#popupModal")) {
+            $("#popupModal").fadeOut();
+        }
+    });
+}
 
-  $(".meet-characters").click(showCharacters);
+$(".meet-characters").click(showCharacters);
 
 // Chapter 1
 
